a5e8c685d6603a7e8b57f050aac0ef73d35ff9ee
Arion Mathias <arion.mathias@gmail.com>
2021-11-06 16:57:00 -0300

    Commit inicial - Adicionando primeiras dependencias

---
+++ src/main/java/br/com/squad4/blue_bank/BlueBankApplication.java
@@ -0,0 +1,13 @@
+package br.com.squad4.blue_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/squad4/blue_bank/BlueBankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.squad4.blue_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
