59ae4066733f530a90cc26f12e5fea6edbb57e17
Eric Monné <ericmfo@hotmail.com>
2021-11-09 22:15:13 -0300

    criação das classes cliente, endereço, telefone e configuração do banco de dados em application.properties

---
+++ src/main/java/br/com/squad4/blue_bank/model/Cliente.java
@@ -0,0 +1,98 @@
+package br.com.squad4.blue_bank.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Column(nullable = false)
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "cliente")
+    private List<Telefone> telefone = new ArrayList<>();
+
+    @Column(nullable = false)
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "cliente")
+    private Endereco endereco;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    public Cliente(String nome, String cpf, Endereco endereco, LocalDate dataNascimento, String email, Telefone telefone) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.dataNascimento = dataNascimento;
+        this.email = email;
+        adicionarTelefone(telefone);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public List<Telefone> getTelefone() {
+        return telefone;
+    }
+
+    public void adicionarTelefone(Telefone telefone) {
+        this.telefone.add(telefone);
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+
+}
