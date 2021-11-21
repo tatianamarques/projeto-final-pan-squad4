ac3606dc43fb8a9f1edd56957ff15ce26b32e7ab
Arion Mathias <arion.mathias@gmail.com>
2021-11-20 22:12:36 -0300

    correcao/ cadastro de cliente

--- src/main/java/br/com/squad4/blue_bank/dto/ClienteDTO.java
+++ src/main/java/br/com/squad4/blue_bank/dto/ClienteDTO.java
@@ -69,6 +69,57 @@ public class ClienteDTO {
 		public void setConta(Long conta) {
 			this.conta = conta;
 		}
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+		public String getCpf() {
+			return cpf;
+		}
+
+		public void setCpf(String cpf) {
+			this.cpf = cpf;
+		}
+
+		public LocalDate getDataNascimento() {
+			return dataNascimento;
+		}
+
+		public void setDataNascimento(LocalDate dataNascimento) {
+			this.dataNascimento = dataNascimento;
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		public TelefoneDTO getTelefoneDTO() {
+			return telefoneDTO;
+		}
+
+		public void setTelefoneDTO(TelefoneDTO telefoneDTO) {
+			this.telefoneDTO = telefoneDTO;
+		}
+
+		public EnderecoDTO getEnderecoDTO() {
+			return enderecoDTO;
+		}
+
+		public void setEnderecoDTO(EnderecoDTO enderecoDTO) {
+			this.enderecoDTO = enderecoDTO;
+		}
+		
+		
+		
 	}
 
 	
--- src/main/java/br/com/squad4/blue_bank/dto/EnderecoDTO.java
+++ src/main/java/br/com/squad4/blue_bank/dto/EnderecoDTO.java
@@ -27,6 +27,10 @@ public class EnderecoDTO {
         this.bairro = bairro;
         this.cidade = cidade;
     }
+    
+    public EnderecoDTO() {
+    	
+    }
 
 
     public Endereco toModel() {
@@ -34,7 +38,48 @@ public class EnderecoDTO {
 
         return new Endereco(this.logradouro, this.numero, this.cep, this.bairro, this.cidade);
     }
+    
+    
 
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
 
 	public String getComplemento() {
 		return complemento;
